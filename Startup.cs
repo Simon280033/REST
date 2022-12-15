767392293b2a61a57a9ca89d0328e3a0d85b58cd
Simon280033 <71387594+Simon280033@users.noreply.github.com>
2022-12-15 20:46:05 +0100

    Code cleanup

--- Startup.cs
+++ Startup.cs
@@ -30,13 +30,9 @@ namespace WebAPI
             services.AddScoped<IActivityContext, ActivityContext>();
             services.AddDbContext<DatabaseContext>(options =>
             {
-                // options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
                 Console.WriteLine("getting to the breakpoint");
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
-
-            // SQLite
-            // services.AddDbContext<DatabaseContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
